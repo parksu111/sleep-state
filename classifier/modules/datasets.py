b2c43356ba9e695bfafbae50069c0a42f3a2cc41
Sungho Park <jy390810@gmail.com>
2022-12-22 01:25:12 +0000

    datasets/initial-commit

---
+++ classifier/modules/datasets.py
@@ -0,0 +1,11 @@
+from torch.utils.data import Dataset
+from modules.utils import load_json
+import numpy as np
+from PIL import Image
+import os
+import pandas as pd
+import torchvision.transforms as transforms
+
+class TraceDataset(Dataset):
+    def __init__(self, img_dir, label_dir):
+        self.df = pd.read_csv(label_dir, )
