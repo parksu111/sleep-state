e78c94dc8828c7f3c61dcc47311b285cfd644f18
Sungho Park <jy390810@gmail.com>
2023-01-11 15:28:23 +0000

    make_data.sh/initial-commit

---
+++ preprocess/make_data.sh
@@ -0,0 +1,4 @@
+python make_im_data.py --output_dir /workspace/Competition/SLEEP/EEG/data/spec1 --datatype spectrogram --num_bins 1
+python make_im_data.py --output_dir /workspace/Competition/SLEEP/EEG/data/spec3 --datatype spectrogram --num_bins 3
+python make_im_data.py --output_dir /workspace/Competition/SLEEP/EEG/data/trace1 --datatype trace --num_bins 1
+python make_im_data.py --output_dir /workspace/Competition/SLEEP/EEG/data/trace3 --datatype trace --num_bins 3
