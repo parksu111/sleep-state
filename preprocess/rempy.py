9cfb1891001a53e6eef6dd3589b0ed9c761fa758
Sungho Park <jy390810@gmail.com>
2023-02-03 03:36:23 +0000

    Add sleep_example plotting function

--- preprocess/rempy.py
+++ preprocess/rempy.py
@@ -161,3 +161,161 @@ def stateSeq(sub, stateInds):
     for x in stateRanges:
         stateSeqs.append(np.arange(x[0],x[1]+1))
     return stateSeqs
+
+'''
+Plot
+'''
+
+def sleep_example(ppath, name, tlegend, tstart, tend, fmax=30, fig_file='', vm=[], ma_thr=10,
+                  fontsize=12, cb_ticks=[], emg_ticks=[], r_mu = [10, 100], fw_color=True):
+    """
+    plot sleep example
+    :param ppath: base folder
+    :param name: recording name
+    :param tstart: start (in seconds) of shown example interval
+    :param tend: end of example interval
+    :param tlegend: length of time legend
+    :param fmax: maximum frequency shown for EEG spectrogram
+    :param fig_file: file name where figure will be saved
+    :param vm: saturation of EEG spectrogram
+    :param fontsize: fontsize
+    :param cb_ticks: ticks for colorbar
+    :param emg_ticks: ticks for EMG amplitude axis (uV)
+    :param r_mu: range of frequencies for EMG amplitude
+    :param fw_color: if True, use standard color scheme for brainstate (gray - NREM, violet - Wake, cyan - REM);
+            otherwise use Shinjae's color scheme
+    """
+    set_fontarial()
+    set_fontsize(fontsize)
+
+    # True, if laser exists, otherwise set to False
+    plaser = False
+
+    sr = get_snr(ppath, name)
+    nbin = np.round(2.5 * sr)
+    dt = nbin * 1 / sr
+
+    istart = int(np.round(tstart/dt))
+    iend   = int(np.round(tend/dt))
+    dur = (iend-istart+1)*dt
+
+    M,K = load_stateidx(ppath, name)
+    #kcut = np.where(K>=0)[0]
+    #M = M[kcut]
+    if tend==-1:
+        iend = len(M)
+    M = M[istart:iend]
+
+    seq = get_sequences(np.where(M==2)[0])
+    for s in seq:
+        if len(s)*dt <= ma_thr:
+            M[s] = 0
+
+    t = np.arange(0, len(M))*dt
+
+    P = so.loadmat(os.path.join(ppath, name, 'sp_%s.mat' % name), squeeze_me=True)
+    SPEEG = P['SP']#/1000000.0
+    # calculate median for choosing right saturation for heatmap
+    med = np.median(SPEEG.max(axis=0))
+    if len(vm) == 0:
+        vm = [0, med*2.5]
+    #t = np.squeeze(P['t'])
+    freq = P['freq']
+    P = so.loadmat(os.path.join(ppath, name, 'msp_%s.mat' % name), squeeze_me=True)
+    SPEMG = P['mSP']#/1000000.0
+
+
+    # create figure
+    plt.ion()
+    plt.figure(figsize=(12,6))
+
+    # axis in the background to draw laser patches
+    axes_back = plt.axes([0.1, .4, 0.8, 0.52])
+    axes_back.get_xaxis().set_visible(False)
+    axes_back.get_yaxis().set_visible(False)
+    axes_back.spines["top"].set_visible(False)
+    axes_back.spines["right"].set_visible(False)
+    axes_back.spines["bottom"].set_visible(False)
+    axes_back.spines["left"].set_visible(False)
+
+    plt.ylim((0,1))
+    plt.xlim([t[0], t[-1]])
+
+
+    # show brainstate
+    axes_brs = plt.axes([0.1, 0.4, 0.8, 0.05])
+    cmap = plt.cm.jet
+    if fw_color:
+        my_map = cmap.from_list('brs', [[1, 118./255, 245./255], [0, 1, 1], [0.6, 0, 1], [0.8, 0.8, 0.8]], 4)
+    else:
+        my_map = cmap.from_list('brs', [[0, 0, 0], [153 / 255.0, 76 / 255.0, 9 / 255.0],
+                                        [120 / 255.0, 120 / 255.0, 120 / 255.0], [1, 0.75, 0]], 4)
+
+    tmp = axes_brs.pcolorfast(t, [0, 1], np.array([M]), vmin=0, vmax=3)
+    tmp.set_cmap(my_map)
+    axes_brs.axis('tight')
+    axes_brs.axes.get_xaxis().set_visible(False)
+    axes_brs.axes.get_yaxis().set_visible(False)
+    axes_brs.spines["top"].set_visible(False)
+    axes_brs.spines["right"].set_visible(False)
+    axes_brs.spines["bottom"].set_visible(False)
+    axes_brs.spines["left"].set_visible(False)
+
+    axes_legend = plt.axes([0.1, 0.33, 0.8, 0.05])
+    plt.ylim((0,1.1))
+    plt.xlim([t[0], t[-1]])
+    plt.plot([0, tlegend], [1, 1], color='black')
+    plt.text(tlegend/4.0, 0.1, str(tlegend) + ' s')
+    axes_legend.spines["top"].set_visible(False)
+    axes_legend.spines["right"].set_visible(False)
+    axes_legend.spines["bottom"].set_visible(False)
+    axes_legend.spines["left"].set_visible(False)
+    axes_legend.axes.get_xaxis().set_visible(False)
+    axes_legend.axes.get_yaxis().set_visible(False)
+
+    # show spectrogram
+    ifreq = np.where(freq <= fmax)[0]
+    # axes for colorbar
+    axes_cbar = plt.axes([0.82, 0.68, 0.1, 0.2])
+    # axes for EEG spectrogram
+    axes_spec = plt.axes([0.1, 0.68, 0.8, 0.2], sharex=axes_brs)
+    im = axes_spec.pcolorfast(t, freq[ifreq], SPEEG[ifreq, istart:iend], cmap='jet', vmin=vm[0], vmax=vm[1])
+    axes_spec.axis('tight')
+    axes_spec.set_xticklabels([])
+    axes_spec.set_xticks([])
+    axes_spec.spines["bottom"].set_visible(False)
+    plt.ylabel('Freq (Hz)')
+    box_off(axes_spec)
+    plt.xlim([t[0], t[-1]])
+
+    # colorbar for EEG spectrogram
+    cb = plt.colorbar(im, ax=axes_cbar, pad=0.0, aspect=10.0)
+    cb.set_label('Power ($\mathrm{\mu}$V$^2$s)')
+    if len(cb_ticks) > 0:
+        cb.set_ticks(cb_ticks)
+    axes_cbar.set_alpha(0.0)
+    axes_cbar.spines["top"].set_visible(False)
+    axes_cbar.spines["right"].set_visible(False)
+    axes_cbar.spines["bottom"].set_visible(False)
+    axes_cbar.spines["left"].set_visible(False)
+    axes_cbar.axes.get_xaxis().set_visible(False)
+    axes_cbar.axes.get_yaxis().set_visible(False)
+
+    # show EMG
+    i_mu = np.where((freq >= r_mu[0]) & (freq <= r_mu[1]))[0]
+    # * 1000: to go from mV to uV
+    p_mu = np.sqrt(SPEMG[i_mu, :].sum(axis=0) * (freq[1] - freq[0])) #* 1000.0 # back to muV
+    axes_emg = plt.axes([0.1, 0.5, 0.8, 0.1], sharex=axes_spec)
+    axes_emg.plot(t, p_mu[istart:iend], color='black')
+    axes_emg.patch.set_alpha(0.0)
+    axes_emg.spines["bottom"].set_visible(False)
+    if len(emg_ticks) > 0:
+        axes_emg.set_yticks(emg_ticks)
+    plt.ylabel('Ampl. ' + '$\mathrm{(\mu V)}$')
+    plt.xlim((t[0], t[-1] + 1))
+    box_off(axes_emg)
+
+    #if len(fig_file) > 0:
+    #    save_figure(fig_file)
+
+    plt.show()
